0f2e517a75ff3d098a8f4684cb9f2fabb9010f82
musfirah zunnoon <musfirahzunnoon@gmail.com>
2025-05-12 18:35:29 +0500

    Kubernetes files added

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*'
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
 
 app.use(express.json());
